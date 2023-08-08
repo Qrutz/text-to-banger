0c6f5d272e24a2a7221601d04886907dd5ad5938
Qrutz <gussanjoba@student.gu.se>
2023-08-08 15:34:46 +0200

    setup + darkmode toggle

---
+++ webapp/app/page.tsx
@@ -0,0 +1,11 @@
+import DarkModeButton from '@/components/DarkModeToggle';
+
+export default function Home() {
+  return (
+    <div className=' min-w-full min-h-screen dark:bg-gray-700 bg-white flex justify-center items-center'>
+      <h1 className='text-4xl font-bold'>text-to-banger</h1>
+
+      <DarkModeButton />
+    </div>
+  );
+}
