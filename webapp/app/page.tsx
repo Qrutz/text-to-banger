f396fa4ff83b60fc52d2bac6fb284d8b1fdef8a7
qrutz <gussanjoba@student.gu.se>
2023-08-12 17:53:51 +0200

    ssh test

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className=' min-w-full min-h-screen dark:bg-gray-700 bg-white flex justify-center items-center'>
       <h1 className='text-4xl font-bold'>text-to-banger</h1>
-
+      sadfasdf
       <DarkModeButton />
     </div>
   );
